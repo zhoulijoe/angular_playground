69cf538146651ca837a63e03ce46dc4c53dcbdef
Zhou Li <zhoulijoe@gmail.com>
2016-07-05 11:27:23 -0700

    Create angular app structure with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myangularApp
+ */
+angular.module('myangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myangularApp
+ */
+angular.module('myangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
